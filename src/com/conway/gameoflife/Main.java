f7f27017ea72dc54d430148b812e958d5ef97e29
Basti Tee <basti.tee@posteo.de>
2018-04-26 13:15:41 +0200

    added conway information; refined mvn targets;

--- src/com/conway/gameoflife/Main.java
+++ src/com/conway/gameoflife/Main.java
@@ -1,4 +1,4 @@
-package com.company.project;
+package com.conway.gameoflife;
 
 public class Main {
 
--- test/com/conway/gameoflife/MainTest.java
+++ test/com/conway/gameoflife/MainTest.java
@@ -1,11 +1,17 @@
-package com.company.project;
+package com.conway.gameoflife;
 
 
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * Test suite for the {@link Main} component.
+ */
 public class MainTest {
 
+    /**
+     * A sample test to validate the overall setup.
+     */
     @Test
     public void testObjectCreation() {
 
