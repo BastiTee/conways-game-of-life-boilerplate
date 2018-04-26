aa2d9959f1c349fa22840007fdf023ade126ac58
Basti Tee <basti.tee@posteo.de>
2018-04-26 11:46:53 +0200

    Added basic project;

---
+++ test/com/company/project/MainTest.java
@@ -0,0 +1,17 @@
+package com.company.project;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testObjectCreation() {
+
+        Main main = new Main();
+        Assert.assertNotNull(main);
+
+    }
+
+}
